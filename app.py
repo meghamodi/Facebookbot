9b3bea906082c63b249825a8bf2849e5f50b904f
Megha Modi <meghamodi007@gmail.com>
2017-05-23 13:34:37 +0530

    Revert "adding files"
    This reverts commit 492a5a2bc78f90b7f8a5a7a5e48419ae395336ff.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os, sys
 from flask import Flask, request
-from utils import wit_response
 from pymessenger import Bot
 
 app = Flask(__name__)
@@ -17,13 +16,12 @@ def verify():
         if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-    return "Hello world", 200
+    return "Hello world", 
 
 @app.route('/', methods=['POST'])
 def webhook():
 	data = request.get_json()
 	log(data)
-
 	if data['object'] == 'page':
 		for entry in data['entry']:
 			for messaging_event in entry['messaging']:
@@ -37,17 +35,7 @@ def webhook():
 					else:
 						messaging_text = 'no text'
 
-					response = None
-
-					entity, value = wit_response(messaging_text)
-
-					if entity == "newstype":
-						response = "OK. I will send you {} news".format(str(value))
-					elif entity == 'location':
-						response = "OK. So you live in {0}. I will send you top headlines from {0}.".format(str(value))
-
-					if response == None:
-					   response = "Sorry.I didn't got you."	
+					response = messaging_text
 					bot.send_text_message(sender_id, response)
 
 	return "ok", 200
